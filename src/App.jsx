da37dfa69b0d0f63bf58c0eb4ac39b7985d3e402
febeitalia <info@febeitalia.it>
2025-06-15 09:52:02 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>Benvenuto su MiniModa 👶</h1>
+      <p>Sito in costruzione - presto disponibile!</p>
+    </div>
+  );
+}
